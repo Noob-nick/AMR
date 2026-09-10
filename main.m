47b3fc13d240b8c8adeecde2261508a4a6e4c2de
Noob-nick <naman27310@gmail.com>
2026-09-10 13:15:12 +0530

    add labelling

--- main.m
+++ main.m
@@ -41,6 +41,31 @@ numsnr=16;
 
 
 
+% labelling the frames  as it is a supervised learning method x=[timematrix freqmatrix ]
+
+combinedfeatures=cell(length(signals),1);
+for m=1:length(signals)
+    combinedfeatures{m}=cell(numsnr,1);
+    for s=1:numsnr
+        x=features{m}{s};%6 *8
+       y=freqfeat{m}{s};%6*8
+       combinedfeatures{m}{s}=[x,y];%6*16
+    end
+end
+modulationnames=["BPSK","QPSK","16QAM","64QAM","8PSK","4PAM","2FSK"];
+labels=cell(length(signals),1);
+for m=1:length(signals)
+    labels{m}=cell(numsnr,1);
+    for s=1:numsnr
+        p=size(combinedfeatures{m}{s},1);
+        labels{m}{s}=categorical(repmat(modulationnames(m),p,1));
+    end
+end
+
+
+
+
+
 
 
 
